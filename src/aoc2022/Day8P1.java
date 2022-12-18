0589de42a877facc50a8574e00b39bc2c06c4e05
Damian Novgorodov <damiannovgorodov@gmail.com>
2022-12-17 21:30:14 -0700

    first commit

---
+++ src/aoc2022/Day8P1.java
@@ -0,0 +1,57 @@
+package aoc2022;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Day8P1 {
+
+    public static final String PATHNAME = "C:\\Users\\Damian\\Documents\\Numbers.txt";
+    public static int visible = 0;
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Path.of(PATHNAME));
+        int[][] trees = new int[lines.size()][lines.get(0).length()];
+        for (int i = 0; i < lines.size(); i++) {
+            for (int j = 0; j < lines.get(i).length(); j++) {
+                trees[i][j] = Integer.parseInt(lines.get(i).charAt(j) + "");
+            }
+        }
+        for (int i = 0; i < lines.size(); i++) {
+            for (int j = 0; j < lines.get(i).length(); j++) {
+                int current = trees[i][j];
+                boolean visibleTop = true;
+                boolean visibleBottom = true;
+                boolean visibleLeft = true;
+                boolean visibleRight = true;
+                for (int k = 0; k < j; k++) {
+                    if (trees[i][k] >= current) {
+                        visibleLeft = false;
+                        break;
+                    }
+                }
+                for (int k = j + 1; k < lines.get(i).length(); k++) {
+                    if (trees[i][k] >= current) {
+                        visibleRight = false;
+                        break;
+                    }
+                }
+                for (int k = 0; k < i; k++) {
+                    if (trees[k][j] >= current) {
+                        visibleTop = false;
+                        break;
+                    }
+                }
+                for (int k = i + 1; k < lines.size(); k++) {
+                    if (trees[k][j] >= current) {
+                        visibleBottom = false;
+                        break;
+                    }
+                }
+                if (visibleTop || visibleBottom || visibleLeft || visibleRight) visible++;
+            }
+        }
+        System.out.println(visible);
+    }
+}
---
+++ src/aoc2022/Day8P2.java
@@ -0,0 +1,54 @@
+package aoc2022;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Day8P2 {
+
+    public static final String PATHNAME = "C:\\Users\\Damian\\Documents\\Numbers.txt";
+    public static int highScore = 0;
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Path.of(PATHNAME));
+        int[][] trees = new int[lines.size()][lines.get(0).length()];
+        for (int i = 0; i < lines.size(); i++) {
+            for (int j = 0; j < lines.get(i).length(); j++) {
+                trees[i][j] = Integer.parseInt(lines.get(i).charAt(j) + "");
+            }
+        }
+        for (int i = 0; i < lines.size(); i++) {
+            for (int j = 0; j < lines.get(i).length(); j++) {
+                int current = trees[i][j];
+                int visibleTop = 0;
+                int visibleBottom = 0;
+                int visibleLeft = 0;
+                int visibleRight = 0;
+                for (int k = j - 1; k >= 0; k--) {
+                    visibleLeft++;
+                    if (trees[i][k] >= current) break;
+                }
+                for (int k = j + 1; k < lines.get(i).length(); k++) {
+                    visibleRight++;
+                    if (trees[i][k] >= current) break;
+                }
+                for (int k = i - 1; k >= 0; k--) {
+                    visibleTop++;
+                    if (trees[k][j] >= current) break;
+                }
+                for (int k = i + 1; k < lines.size(); k++) {
+                    visibleBottom++;
+                    if (trees[k][j] >= current) break;
+                }
+
+                int scenicScore = visibleLeft * visibleRight * visibleTop * visibleBottom;
+                if (scenicScore > highScore) {
+                    //System.out.println(i + " " + j);
+                    highScore = scenicScore;
+                }
+            }
+        }
+        System.out.println(highScore);
+    }
+}
