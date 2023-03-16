80d438c5b57df1ba891be2e9eec7c6f72274ee9e
Damian Novgorodov <damiannovgorodov@gmail.com>
2023-03-16 15:43:45 -0700

    Major revamp to code.

--- src/aoc2022/Day9P2.java
+++ src/aoc2022/Day9P2.java
@@ -9,100 +9,51 @@ import java.util.List;
 public class Day9P2 {
 
     public static final String PATHNAME = "inputs\\day9.txt";
+    private static final Point[] rope = new Point[10];
+    private static final HashSet<String> tailCount = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Path.of(PATHNAME));
-        String[] rope = new String[10];
-        HashSet<String> tailCount = new HashSet<>();
-        tailCount.add("0, 0");
         for (int i = 0; i < 10; i++) {
-            rope[i] = ("0, 0");
+            rope[i] = new Point(0, 0);
         }
         for (String line : lines) {
             String direction = line.split(" ")[0];
-            String current = rope[0];
-            String currentX = getCurrentX(current);
-            String currentY = getCurrentY(current);
             int distance = Integer.parseInt(line.split(" ")[1]);
-            int xValue = Integer.parseInt(currentX);
-            int yValue = Integer.parseInt(currentY);
             for (int i = 0; i < distance; i++) {
                 switch (direction) {
-                    case "U" -> yValue++;
-                    case "D" -> yValue--;
-                    case "L" -> xValue--;
-                    default -> xValue++;
+                    case "U" -> rope[0].y++;
+                    case "D" -> rope[0].y--;
+                    case "L" -> rope[0].x--;
+                    case "R" -> rope[0].x++;
                 }
-                rope[0] = (xValue + ", " + yValue);
-                findTailOccurrences(rope, tailCount);
+                findTailOccurrences();
             }
         }
         System.out.println(tailCount.size());
     }
 
-    private static void findTailOccurrences(String[] rope, HashSet<String> tailCount) {
+    private static void findTailOccurrences() {
         for (int k = 0; k < rope.length - 1; k++) {
-            if (isTouching(rope[k], rope[k + 1])) {
-                if (isInRowOrColumn(rope[k], rope[k + 1])) moveDiagonally(rope, k);
-                else moveStraight(rope, k);
+            Point currentKnot = rope[k];
+            Point nextKnot = rope[k + 1];
+            if (Math.abs(currentKnot.x - nextKnot.x) > 1 || Math.abs(currentKnot.y - nextKnot.y) > 1) {
+                nextKnot.x += Integer.signum(currentKnot.x - nextKnot.x);
+                nextKnot.y += Integer.signum(currentKnot.y - nextKnot.y);
             }
-            else break;
         }
-        String latestPosition = rope[9];
-        tailCount.add(latestPosition);
+        Point latestPosition = rope[9];
+        tailCount.add(latestPosition.x + ", " + latestPosition.y);
     }
 
-    public static boolean isTouching(String previous, String next) {
-        String currentTailX = getCurrentX(previous);
-        String currentTailY = getCurrentY(previous);
-        String currentHeadX = getCurrentX(next);
-        String currentHeadY = getCurrentY(next);
-        int headMinusTailX = Integer.parseInt(currentHeadX) - Integer.parseInt(currentTailX);
-        int headMinusTailY = Integer.parseInt(currentHeadY) - Integer.parseInt(currentTailY);
-        return Math.abs(headMinusTailX) > 1 || Math.abs(headMinusTailY) > 1;
-    }
+}
 
-    public static boolean isInRowOrColumn(String previous, String next) {
-        String currentTailX = getCurrentX(previous);
-        String currentTailY = getCurrentY(previous);
-        String currentHeadX = getCurrentX(next);
-        String currentHeadY = getCurrentY(next);
-        int headMinusTailX = Integer.parseInt(currentHeadX) - Integer.parseInt(currentTailX);
-        int headMinusTailY = Integer.parseInt(currentHeadY) - Integer.parseInt(currentTailY);
-        return headMinusTailX != 0 && headMinusTailY != 0;
-    }
+class Point {
 
-    public static String getCurrentX(String knot) {
-        return knot.split(", ")[0];
-    }
+    int x, y;
 
-    public static String getCurrentY(String knot) {
-        return knot.split(", ")[1];
-    }
-
-    public static void moveStraight(String[] rope, int index) {
-        int currentX = Integer.parseInt(getCurrentX(rope[index]));
-        int nextX = Integer.parseInt(getCurrentX(rope[index + 1]));
-        int currentY = Integer.parseInt(getCurrentY(rope[index]));
-        int nextY = Integer.parseInt(getCurrentY(rope[index + 1]));
-        if (currentX == nextX) {
-            if (currentY > nextY) rope[index + 1] = (nextX + ", " + (nextY + 1));
-            else rope[index + 1] = (nextX + ", " + (nextY - 1));
-        }
-        else if (currentY == nextY) {
-            if (currentX > nextX) rope[index + 1] = ((nextX + 1) + ", " + nextY);
-            else rope[index + 1] = ((nextX - 1) + ", " + nextY);
-        }
-    }
-
-    public static void moveDiagonally(String[] rope, int index) {
-        int currentX = Integer.parseInt(getCurrentX(rope[index]));
-        int nextX = Integer.parseInt(getCurrentX(rope[index + 1]));
-        int currentY = Integer.parseInt(getCurrentY(rope[index]));
-        int nextY = Integer.parseInt(getCurrentY(rope[index + 1]));
-        if (currentX > nextX && currentY > nextY) rope[index + 1] = ((nextX + 1) + ", " + (nextY + 1));
-        else if (currentX < nextX && currentY > nextY) rope[index + 1] = ((nextX - 1) + ", " + (nextY + 1));
-        else if (currentX > nextX && currentY < nextY) rope[index + 1] = ((nextX + 1) + ", " + (nextY - 1));
-        else rope[index + 1] = ((nextX - 1) + ", " + (nextY - 1));
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
